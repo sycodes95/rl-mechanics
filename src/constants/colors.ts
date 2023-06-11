f2acfee7620494b60eb9489d429a6501dcca2a9b
sycodes95 <sycodes95@gmail.com>
2023-06-11 04:12:00 -0700

    added homepage to package.json

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,7 +4,7 @@ export const difficultyColors: {[key: number] : string} = {
   2 : 'text-green-400',
   3 : 'text-yellow-400',
   4 : 'text-orange-400',
-  5: 'text-red-500',
+  5 : 'text-red-500',
 }
 
 export const importanceColors: {[key: number] : string} = {
