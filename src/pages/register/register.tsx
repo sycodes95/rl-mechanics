12ec19ff553e082c777034205c83a49b11c5f627
sycodes95 <sycodes95@gmail.com>
2023-07-02 22:29:34 -0700

    replaced register google log in with correct url

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -180,7 +180,7 @@ function Register () {
           </div>
           }
           <p className="flex justify-center text-xs text-white">OR</p>
-          <button className="flex items-center h-10 gap-4 p-4 text-sm text-gray-800 bg-white rounded-lg" onClick={()=> window.open('http://localhost:5000/auth/google')}>
+          <button className="flex items-center h-10 gap-4 p-4 text-sm text-gray-800 bg-white rounded-lg" onClick={()=> window.open(`${import.meta.env.VITE_API_HOST_URL}/auth/google`)}>
             <ReactSVG className="" src={googlesvg} />
             <p className="text-gray-600 text-md">Continue with Google</p>
           </button>
