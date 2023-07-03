d760e0a6088b7badbf2a11296199cd3695204fac
sycodes95 <sycodes95@gmail.com>
2023-07-02 21:51:30 -0700

    put in correct path for google login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -156,7 +156,7 @@ function Login (){
 
           <p className="flex justify-center text-xs text-white">OR</p>
 
-          <button className="flex items-center h-10 gap-4 p-4 text-sm text-gray-800 bg-white rounded-lg" onClick={()=> window.open(`${import.meta.env.VITE_API_HOST_URL}`)}>
+          <button className="flex items-center h-10 gap-4 p-4 text-sm text-gray-800 bg-white rounded-lg" onClick={()=> window.open(`${import.meta.env.VITE_API_HOST_URL}/auth/google`)}>
             <ReactSVG className="" src={googlesvg} />
             <p className="text-gray-600 text-md">Continue with Google</p>
           </button>
